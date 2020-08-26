67e94ef91bac9068a1bf166ff0b40ed9e05d4249
Linda Johnstone <linda.johnstone@myob.com>
2020-08-26 14:20:07 +1000

    Regular expressions examples 26/8

---
+++ RegularExpressions/RegexOneReplaceString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexOneReplaceString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lets try and reverse the order of the day and month in a few date 
+            // strings. Notice how the replacement string also contains metacharacters
+            // (the back references to the captured groups) so we use a verbatim 
+            // string for that as well.
+            string pattern = @"([a-zA-Z]+) (\d+)";
+
+            // This will reorder the string inline and print:
+            //   24 of June, 9 of August, 12 of Dec
+            // Remember that the first group is always the full matched text, so the 
+            // month and day indices start from 1 instead of zero.
+            string replacedString = Regex.Replace("June 24, August 9, Dec 12",
+                pattern, @"$2 of $1");
+            Console.WriteLine(replacedString);
+        }
+    }
+}
---
+++ RegularExpressions/ZetCodeIsMatchRegex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string>() { "Seven", "even",
+                    "Maven", "Amen", "eleven" };
+            /* 
+                The RegexOptions.Compiled option specifies that the regular expression is compiled to an assembly. 
+                This yields faster execution but increases startup time. 
+            */
+            var rx = new Regex(@".even", RegexOptions.Compiled); 
+
+            foreach (string word in words)
+            {
+                if (rx.IsMatch(word))
+                {
+                    Console.WriteLine($"{word} does match");
+                }
+                else
+                {
+                    Console.WriteLine($"{word} does not match");
+                }
+            }
+        }
+    }
+}
